d4369fd89af4bf9855417a38295f4eb17fab735f
GabrielCoelho <gbcoe@outlook.com.br>
2024-05-21 20:05:56 -0300

    fix(mentirinha.c): Correção do exercício
    Embora não houvesse em nenhum lugar do caderno de questões, alguns
    exercícios, para serem validados, solicitavam a quebra de linha. Este
    foi um deles

--- IDK_Programming_Codes/Problema K/mentirinha.c
+++ IDK_Programming_Codes/Problema K/mentirinha.c
@@ -6,15 +6,15 @@ int main() {
     scanf("%d", &N);
 
     if( N <= 1 || N >= 1000000){
-        printf("nao");
+        printf("nao\n");
         return 0;
     }
     int raiz = sqrt(N);
 
     if (raiz * raiz == N) {
-        printf("sim");
+        printf("sim\n");
     } else {
-        printf("nao");
+        printf("nao\n");
     }
 
     return 0;
